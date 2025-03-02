7164879eb625e6e051351d33db772a557d39da33
SR <sagarzadafiya4743@gmail.com>
2025-03-02 18:24:27 +0530

    dsaas

--- lib/app/modules/auth/add_company_name_address/views/add_company_name_address_view.dart
+++ lib/app/modules/auth/add_company_name_address/views/add_company_name_address_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:jumla/app/resources/app_assets.dart';
 import 'package:jumla/app/resources/app_colors.dart';
 import 'package:jumla/app/resources/app_styles.dart';
+import 'package:jumla/app/routes/app_pages.dart';
 
 import '../../../../common/common_text_field.dart';
 import '../controllers/add_company_name_address_controller.dart';
@@ -22,7 +22,8 @@ class AddCompanyNameAddressView
             SizedBox(width: 10),
             Text(
               "Company name & address",
-              style: AppTextStyles.bold(fontSize: 18.0, fontColor: AppColors.whiteColor),
+              style: AppTextStyles.bold(
+                  fontSize: 18.0, fontColor: AppColors.whiteColor),
             ),
           ],
         ),
@@ -31,20 +32,31 @@ class AddCompanyNameAddressView
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          CommonTextFieldWithTitle(label: "Company name", controller: controller.companyNameController),
-          CommonTextFieldWithTitle(label: "Address", controller: controller.addressController),
-          CommonTextFieldWithTitle(label: "Contact number", controller: controller.contactNumberController),
-          CommonTextFieldWithTitle(label: "Email", controller: controller.emailController),
-          CommonTextFieldWithTitle(label: "Website", controller: controller.websiteController,hint: "http://mycompany.com",),
-          CommonTextFieldWithTitle(label: "Slogan", controller: controller.sloganController),
+          CommonTextFieldWithTitle(
+              label: "Company name",
+              controller: controller.companyNameController),
+          CommonTextFieldWithTitle(
+              label: "Address", controller: controller.addressController),
+          CommonTextFieldWithTitle(
+              label: "Contact number",
+              controller: controller.contactNumberController),
+          CommonTextFieldWithTitle(
+              label: "Email", controller: controller.emailController),
+          CommonTextFieldWithTitle(
+            label: "Website",
+            controller: controller.websiteController,
+            hint: "http://mycompany.com",
+          ),
+          CommonTextFieldWithTitle(
+              label: "Slogan", controller: controller.sloganController),
         ],
       ),
       bottomNavigationBar: Container(
         color: AppColors.blueColor,
         padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
         child: GestureDetector(
-          onTap: (){
-
+          onTap: () {
+            Get.toNamed(Routes.HOME);
           },
           child: SizedBox(
             width: double.infinity,
@@ -52,7 +64,8 @@ class AddCompanyNameAddressView
             child: Center(
               child: Text(
                 "NEXT",
-                style: AppTextStyles.bold(fontSize: 18.0, fontColor: AppColors.whiteColor),
+                style: AppTextStyles.bold(
+                    fontSize: 18.0, fontColor: AppColors.whiteColor),
               ),
             ),
           ),
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
@@ -13,12 +12,58 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+      body: InkWell(
+        onTap: () {
+          _showPopup();
+        },
+        child: const Center(
+          child: Text(
+            'HomeView is working',
+            style: TextStyle(fontSize: 20),
+          ),
         ),
       ),
+    );
+  }
+
+  void _showPopup() {
+    Get.defaultDialog(
+      radius: 0.0,
+      title: "More shortcuts",
+      titleStyle: TextStyle(
+          fontSize: 20, fontWeight: FontWeight.bold, color: Colors.blue),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Divider(thickness: 1, color: Colors.blue),
+          Column(
+            children: ['ds', 'dsdsa', 'dsd'].map((key) {
+              return CheckboxListTile(
+                title: Text(key, style: TextStyle(fontWeight: FontWeight.bold)),
+                value: true,
+                onChanged: (bool? value) {
+                  // controller.shortcuts[key] = value ?? false;
+                },
+                activeColor: Colors.blue,
+                checkColor: Colors.white,
+              );
+            }).toList(),
+          ),
+          SizedBox(height: 10),
+          ElevatedButton(
+            onPressed: () => Get.back(),
+            child: Text("Done"),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMenuItem(String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Text(title, style: TextStyle(fontSize: 16)),
     );
   }
 }
