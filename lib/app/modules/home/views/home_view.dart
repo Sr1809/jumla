8689f3f28cff44ab25634e212be497d4b8e0ce7d
auxano <jaymeen.meghani@auxanoglobalservices.com>
2025-03-02 18:23:23 +0530

    home screen app bar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:jumla/app/resources/app_assets.dart';
+import 'package:jumla/app/resources/app_colors.dart';
 
+import '../../../resources/app_styles.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
@@ -9,15 +12,30 @@ class HomeView extends GetView<HomeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.whiteColor,
       appBar: AppBar(
-        title: const Text('HomeView'),
-        centerTitle: true,
-      ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            Image.asset(AppAssets.logo,  width: 100),
+
+          ],
         ),
+        backgroundColor: AppColors.blueColor,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.sync, color: Colors.red),
+            onPressed: () {
+              // Handle refresh
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.menu, color: AppColors.whiteColor),
+            onPressed: () {
+              // Handle menu
+            },
+          ),
+        ],
       ),
     );
   }
